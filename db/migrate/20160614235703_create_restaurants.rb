b4472084b52e5aaca685b18e160ae558fd711d3f
MiguelAngelVM <MiguelAngelVM>
2016-06-16 22:33:45 -0500

    Revert "Migracion Restaurant"
    This reverts commit 42e061825289a5b9eee1eac59a2ade91ee76e6a8.

--- db/migrate/20160614235703_create_restaurants.rb
+++ db/migrate/20160614235703_create_restaurants.rb
@@ -1,9 +1,9 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
-      t.string :name
-      t.text :description
-  	  t.string :location
+      t.string :commenter
+      t.text :body
+      t.references :article, index: true, foreign_key: true
 
       t.timestamps null: false
     end
