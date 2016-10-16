425258477d3ca1b4fc11d68dd8c2c6a84ee097f2
MiguelAngelVM <MiguelAngelVM>
2016-10-16 13:58:27 -0500

    mapa con llave expirada

--- app/assets/javascripts/modalMap.js
+++ app/assets/javascripts/modalMap.js
@@ -1,138 +1,136 @@
-/*
-var directionsService = new google.maps.DirectionsService();
-var directionsDisplay = new google.maps.DirectionsRenderer();
-
-function calcRoute() {
-  var origin      = new google.maps.LatLng(31.8465264,-116.65861,12);
-  var destination = new google.maps.LatLng(31.8465264,-116.65861,12);
-  var request = {
-      origin:      origin,
-      destination: destination,
-      travelMode:  google.maps.TravelMode.DRIVING
-  };
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    }
-  });
-}
-
-calcRoute();
-
-var handler = Gmaps.build('Google');
-handler.buildMap({
- internal: {id: 'directions'}
-}, function(){
-  directionsDisplay.setMap(handler.getMap());
-
-});
-*/
-
 $(document).ready(function(){
-    scrollA();
+		scrollA();
 	var modal = document.getElementById('myModal');
 	var span = document.getElementsByClassName("close")[0];
 	var allSite = document.getElementById('allSite').value;
 	for (var i = 1; i <= parseInt(allSite); i++) {
 		var btn = document.getElementById("btnModel"+i);
 		btn.onclick = function() {
-		    modal.style.display = "block";
+				modal.style.display = "block";
 		}
 	}
 	span.onclick = function() {
-	    modal.style.display = "none";
+			modal.style.display = "none";
 	}
 	window.onclick = function(event) {
-	    if (event.target == modal) {
-	        modal.style.display = "none";
-	    }
+			if (event.target == modal) {
+					modal.style.display = "none";
+			}
 	}
 	 $('.kv-gly-star').rating({
-            containerClass: 'is-star'
-        });
-        $('.kv-gly-heart').rating({
-            containerClass: 'is-heart',
-            defaultCaption: '{rating} hearts',
-            starCaptions: function (rating) {
-                return rating == 1 ? 'One heart' : rating + ' hearts';
-            },
-            filledStar: '<i class="glyphicon glyphicon-heart"></i>',
-            emptyStar: '<i class="glyphicon glyphicon-heart-empty"></i>'
-        });
-        $('.kv-fa').rating({
-            theme: 'krajee-fa',
-            filledStar: '<i class="fa fa-star"></i>',
-            emptyStar: '<i class="fa fa-star-o"></i>'
-        });
-        $('.kv-fa-heart').rating({
-            defaultCaption: '{rating} hearts',
-            starCaptions: function (rating) {
-                return rating == 1 ? 'One heart' : rating + ' hearts';
-            },
-            theme: 'krajee-fa',
-            filledStar: '<i class="fa fa-heart"></i>',
-            emptyStar: '<i class="fa fa-heart-o"></i>'
-        });
-        $('.kv-uni-star').rating({
-            theme: 'krajee-uni',
-            filledStar: '&#x2605;',
-            emptyStar: '&#x2606;'
-        });
-        $('.kv-uni-rook').rating({
-            theme: 'krajee-uni',
-            defaultCaption: '{rating} rooks',
-            starCaptions: function (rating) {
-                return rating == 1 ? 'One rook' : rating + ' rooks';
-            },
-            filledStar: '&#9820;',
-            emptyStar: '&#9814;'
-        });
-        $('.kv-svg').rating({
-            theme: 'krajee-svg',
-            filledStar: '<span class="krajee-icon krajee-icon-star"></span>',
-            emptyStar: '<span class="krajee-icon krajee-icon-star"></span>'
-        });
-        $('.kv-svg-heart').rating({
-            theme: 'krajee-svg',
-            filledStar: '<span class="krajee-icon krajee-icon-heart"></span>',
-            emptyStar: '<span class="krajee-icon krajee-icon-heart"></span>',
-            defaultCaption: '{rating} hearts',
-            starCaptions: function (rating) {
-                return rating == 1 ? 'One heart' : rating + ' hearts';
-            },
-            containerClass: 'is-heart'
-        });
+						containerClass: 'is-star'
+				});
+				$('.kv-gly-heart').rating({
+						containerClass: 'is-heart',
+						defaultCaption: '{rating} hearts',
+						starCaptions: function (rating) {
+								return rating == 1 ? 'One heart' : rating + ' hearts';
+						},
+						filledStar: '<i class="glyphicon glyphicon-heart"></i>',
+						emptyStar: '<i class="glyphicon glyphicon-heart-empty"></i>'
+				});
+				$('.kv-fa').rating({
+						theme: 'krajee-fa',
+						filledStar: '<i class="fa fa-star"></i>',
+						emptyStar: '<i class="fa fa-star-o"></i>'
+				});
+				$('.kv-fa-heart').rating({
+						defaultCaption: '{rating} hearts',
+						starCaptions: function (rating) {
+								return rating == 1 ? 'One heart' : rating + ' hearts';
+						},
+						theme: 'krajee-fa',
+						filledStar: '<i class="fa fa-heart"></i>',
+						emptyStar: '<i class="fa fa-heart-o"></i>'
+				});
+				$('.kv-uni-star').rating({
+						theme: 'krajee-uni',
+						filledStar: '&#x2605;',
+						emptyStar: '&#x2606;'
+				});
+				$('.kv-uni-rook').rating({
+						theme: 'krajee-uni',
+						defaultCaption: '{rating} rooks',
+						starCaptions: function (rating) {
+								return rating == 1 ? 'One rook' : rating + ' rooks';
+						},
+						filledStar: '&#9820;',
+						emptyStar: '&#9814;'
+				});
+				$('.kv-svg').rating({
+						theme: 'krajee-svg',
+						filledStar: '<span class="krajee-icon krajee-icon-star"></span>',
+						emptyStar: '<span class="krajee-icon krajee-icon-star"></span>'
+				});
+				$('.kv-svg-heart').rating({
+						theme: 'krajee-svg',
+						filledStar: '<span class="krajee-icon krajee-icon-heart"></span>',
+						emptyStar: '<span class="krajee-icon krajee-icon-heart"></span>',
+						defaultCaption: '{rating} hearts',
+						starCaptions: function (rating) {
+								return rating == 1 ? 'One heart' : rating + ' hearts';
+						},
+						containerClass: 'is-heart'
+				});
 
-        $('.rating,.kv-gly-star,.kv-gly-heart,.kv-uni-star,.kv-uni-rook,.kv-fa,.kv-fa-heart,.kv-svg,.kv-svg-heart').on(
-                'change', function () {
-                    console.log('Rating selected: ' + $(this).val());
-                });
-
+				$('.rating,.kv-gly-star,.kv-gly-heart,.kv-uni-star,.kv-uni-rook,.kv-fa,.kv-fa-heart,.kv-svg,.kv-svg-heart').on(
+								'change', function () {
+										console.log('Rating selected: ' + $(this).val());
+								});
+				map();
 
 });
 
 function scrollA()
 {
-     $('a[href*=#]').click(function() {
+		 $('a[href*=#]').click(function() {
 
-     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-         && location.hostname == this.hostname) {
+			 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+				 && location.hostname == this.hostname) {
 
-             var $target = $(this.hash);
+				 var $target = $(this.hash);
 
-             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+			 $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
 
-             if ($target.length) {
+			 if ($target.length) {
 
-                 var targetOffset = $target.offset().top;
+				 var targetOffset = $target.offset().top;
 
-                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+				 $('html,body').animate({scrollTop: targetOffset}, 1000);
 
-                 return false;
+				 return false;
 
-            }
+			 }
 
-       }
+		 }
 
-   });
+	 });
+}
+	 
+function map()
+{
+	var directionsService = new google.maps.DirectionsService();
+	var directionsDisplay = new google.maps.DirectionsRenderer();
+
+	function calcRoute() {
+		var origin      = new google.maps.LatLng(31.8465264,-116.65861,12);
+		var destination = new google.maps.LatLng(31.8465264,-116.65861,12);
+		var request = {
+				origin:      origin,
+				destination: destination,
+				travelMode:  google.maps.TravelMode.DRIVING
+		};
+		directionsService.route(request, function(response, status) {
+			if (status == google.maps.DirectionsStatus.OK) {
+				directionsDisplay.setDirections(response);
+			}
+		});
+	}
+	calcRoute();
+	var handler = Gmaps.build('Google');
+	handler.buildMap({
+	internal: {id: 'directions'}
+	}, function(){
+		directionsDisplay.setMap(handler.getMap());
+	});
 }
