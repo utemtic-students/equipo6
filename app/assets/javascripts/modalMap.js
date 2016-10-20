89e9e70fbb2e9e9f6163c92567cfb4b408d32904
MiguelAngelVM <MiguelAngelVM>
2016-10-19 22:14:19 -0500

    marge

--- app/assets/javascripts/modalMap.js
+++ app/assets/javascripts/modalMap.js
@@ -1,31 +1,5 @@
 $(document).ready(function(){
-	scrollA();
-	window.myMap =  function () {           
-	  var map = new google.maps.Map(document.getElementById('map'), {
-	    center: {lat: -34.397, lng: 150.644},
-	    zoom: 6
-	  });
-	  var infoWindow = new google.maps.InfoWindow({map: map});
-
-	  // Try HTML5 geolocation.
-	  if (navigator.geolocation) {
-	    navigator.geolocation.getCurrentPosition(function(position) {
-	      var pos = {
-	        lat: position.coords.latitude,
-	        lng: position.coords.longitude
-	      };
-
-	      infoWindow.setPosition(pos);
-	      infoWindow.setContent('Location found.');
-	      map.setCenter(pos);
-	    }, function() {
-	      handleLocationError(true, infoWindow, map.getCenter());
-	    });
-	  } else {
-	    // Browser doesn't support Geolocation
-	    handleLocationError(false, infoWindow, map.getCenter());
-	  }
-	}
+		scrollA();
 	var modal = document.getElementById('myModal');
 	var span = document.getElementsByClassName("close")[0];
 	var allSite = document.getElementById('allSite').value;
@@ -103,6 +77,7 @@ $(document).ready(function(){
 								'change', function () {
 										console.log('Rating selected: ' + $(this).val());
 								});
+
 });
 
 function scrollA()
@@ -130,3 +105,4 @@ function scrollA()
 
 	 });
 }
+	 
