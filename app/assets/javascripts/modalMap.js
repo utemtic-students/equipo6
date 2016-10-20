57d71d41a99b2e77c2c5a34c215aaa6e9e7246f8
MiguelAngelVM <MiguelAngelVM>
2016-10-19 23:52:46 -0500

    Se agrego el mapa en el detalle - Se arreglo el fix de inicio con las imagenes

--- app/assets/javascripts/modalMap.js
+++ app/assets/javascripts/modalMap.js
@@ -1,22 +1,6 @@
 $(document).ready(function(){
 		scrollA();
-	var modal = document.getElementById('myModal');
-	var span = document.getElementsByClassName("close")[0];
 	var allSite = document.getElementById('allSite').value;
-	for (var i = 1; i <= parseInt(allSite); i++) {
-		var btn = document.getElementById("btnModel"+i);
-		btn.onclick = function() {
-				modal.style.display = "block";
-		}
-	}
-	span.onclick = function() {
-			modal.style.display = "none";
-	}
-	window.onclick = function(event) {
-			if (event.target == modal) {
-					modal.style.display = "none";
-			}
-	}
 	 $('.kv-gly-star').rating({
 						containerClass: 'is-star'
 				});
